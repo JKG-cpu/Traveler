a21ed4aee9d9315a44eec650ec89103a13103208
JKG <jkg66512@gmail.com>
2025-12-08 21:32:25 -0800

    Completed GameLoader's display_game_details method

--- Traveler/settings.py
+++ Traveler/settings.py
@@ -1,5 +1,6 @@
 import json
 import threading
+import random
 
 from VividText import VividText as vt
 from rich.console import Console
@@ -77,45 +78,74 @@ class GameLoader(DataLoader):
         self.game_options = {
             "Difficulty": {
                 "Easy": {
-                    "Starting Cash": 1000,
+                    "Starting Cash": "$1000",
                     "Starting Food": "75 lbs"
                 },
                 "Medium": {
-                    "Starting Cash": 500,
+                    "Starting Cash": "$500",
                     "Starting Food": "50 lbs"
                 },
                 "Hard": {
-                    "Starting Cash": 250,
+                    "Starting Cash": "$250",
                     "Starting Food": "25 lbs"
                 }
             },
             "Difficulty-Selection": "Easy",
             "Name": None,
-            "Character-Name": None
+            "Character": {
+                "Name": None,
+                "Age": None,
+                "Gender": None
+            }
+        }
+        self.name_options = {
+            "Male": ["James", "Michael", "William", "David", "Daniel"],
+            "Female": ["Emily", "Olivia", "Sophia", "Ava", "Isabella"]
         }
 
     def display_game_details(self, details: dict):
+        # Game Details
         difficulty = f"Difficulty: {details["Difficulty-Selection"]}"
 
-        line = "+--" + ("-" * max(len(details["Name"] if details["Name"] else "New Save"), len(difficulty))) + "--+"
+        game_line = "+--" + ("-" * max(len(details["Name"] if details["Name"] else "New Save"), len(difficulty))) + "--+"
+        full_game_line = "+--" + (("-" * (len(game_line) - 3)) + "---" + ("-" * (len(game_line) - 3))) + "--+"
 
-        # Name
-        main_vt.print(line)
-        main_vt.print(details["Name"].center(len(line), " ") if details["Name"] else "New Save".center(len(line), " "))
-        main_vt.print(line)
+        main_vt.print(game_line, end = "   ")
+        main_vt.print(game_line)
+        main_vt.print(details["Name"].center(len(game_line)) if details["Name"] else "New Save".center(len(game_line)), end = "   ")
+        main_vt.print(difficulty.center(len(game_line)))
+        main_vt.print(game_line, end = "   ")
+        main_vt.print(game_line)
+        print()
 
-        # Difficulty
-        main_vt.print(line)
-        main_vt.print(difficulty.center(len(line), " "))
-        main_vt.print(line)
+        section = details["Difficulty"][details["Difficulty-Selection"]]
 
-        section = details["Difficulty"][difficulty]
-
+        main_vt.print(full_game_line)
+        main_vt.print("  Settings ".center(len(full_game_line)))
+        main_vt.print(full_game_line)
         main_vt.print(f"   - Starting Cash: {section["Starting Cash"]}")
         main_vt.print(f"   - Starting Food: {section["Starting Food"]}")
         main_vt.print("")
 
-        # Character
+        # Character Details
+        character = details["Character"]
+
+        name_text = f"Name: {character["Name"]}" if character["Name"] else f"Name: {random.choice(self.name_options.get(character["Gender"], self.name_options[random.choice(["Male", "Female"])]))}"
+        age_text = f"Age: {character["Age"]}" if character["Age"] else "Age: 20"
+        gender_text = f"Gender: {character["Gender"]}" if character["Gender"] else "Gender: Male"
+
+        char_line = "+--" + (
+            "-" * len(name_text) + "---" +
+            "-" * len(age_text) + "---" + 
+            "-" * len(gender_text)
+        ) + "--+"
+
+        main_vt.print(char_line)
+        character_dis = "Character".center(len(char_line))
+        main_vt.print(f"[italic]{character_dis}[/]")
+        main_vt.print(char_line)
+        main_vt.print(f"   {name_text}   {age_text}   {gender_text}   ")
+        main_vt.print(char_line)
 
     def create_new_game(self) -> None:
         running = True
